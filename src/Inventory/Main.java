8ebaeb7456d726bf5742bc6e8fd47e734998a59c
jxc6125 <Esmeralda@owners-MacBook-Pro.local>
2019-06-07 13:02:37 -0400

    Initial commit

---
+++ src/Inventory/Main.java
@@ -0,0 +1,52 @@
+package Inventory;
+
+
+        public class car {
+            private String make;
+            private String model;
+            private int year;
+            private String color;
+            private double price;
+            private double mpg;
+            private int mileage;
+        }
+        public String getMake(){
+        }
+        public void setMake(String make) {
+        }
+        public String getModel(){
+        }
+        public void setModel(String model) {
+        }
+        public int getYear() {
+        }
+        public void int setYear(int year) {
+        }
+        public String getColor() {
+        }
+        public void String setColor(String color) {
+        }
+        public double getPrice() {
+        }
+        public void setPrice(double price) {
+        }
+        public double getMPG(){
+        }
+        public void setMPG(double mpg) {
+        }
+
+        public int getMileage() {
+        }
+        public void setMileage() {
+        }
+
+        public class Inventory {
+            Private ArrayList <Car> cars;
+        }
+        public void addCar(){
+        }
+        public void removeCar(){
+        }
+        public void carSold() {
+        }    }
+}
